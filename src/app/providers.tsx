8bb83e49c319c252c03aafb9ba2d0f9bb0180442
Kukuliak Dmytro <kukuliak.dmytro@gmail.com>
2025-10-24 11:15:36 +0300

    feat: add theme switching
    - Added new dependencies: @radix-ui/react-dropdown-menu and next-themes.
    - Updated layout to wrap children with Providers for improved context management.
    - Adjusted package-lock.json to reflect new package versions and dependencies.

---
+++ src/app/providers.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ModeToggle } from "@/features/theme-switcher/theme-switcher";
+export function Providers({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system">
+      <ModeToggle />
+      {children}
+    </NextThemesProvider>
+  );
+}
